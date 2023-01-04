17af2982e72d002ab6b4d64995bbf24bd86636bc
Arshak <arshakmohammed004@gmail.com>
2023-01-05 01:19:04 +0530

    one way data binding-string interpolation,property binding and event binding

--- src/app/notification/notification.component.ts
+++ src/app/notification/notification.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification',
-  template:`<div class="alert alert-success">
-              <p>This website uses cookies to provide better user experince.</p>
+  template: `<div class="alert alert-success" [hidden]="displayNotification">
+              This website uses cookies to provide better user experince.
+              <div class="close"><button class="btn" (click)="closeNotification()">X</button></div>
             </div>`,
-  styles:[".notification-div{margin: 10px 0px; padding:10px 20px;background-color:#FAD7A0;text-align:center;}", "p{font-size:14px;  }"]
+  styles: [".notification-div{margin: 10px 0px; padding:10px 20px;background-color:#FAD7A0;text-align:center;}",
+    ".close{float:right; margin-top:-15px;}"]
 })
 export class NotificationComponent {
-
+  displayNotification: boolean = false;
+  closeNotification(){
+    this.displayNotification=true;
+  }
 }
